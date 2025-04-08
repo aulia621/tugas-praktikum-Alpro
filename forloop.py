833f2b780dd1eb6d2eb9b24d369931118ce7d970
aulia621 <izzaauliam@gmail.com>
2025-04-08 19:27:39 +0700

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,13 @@
+# contoh 1
+print('for in sequence')
+nama =['alfy','alfan','ana','amar']
+for data in nama :
+    print('nama:', data)
+print("==================")
+ 
+# contoh 2
+print('for in range')
+for nilai in range(1,11):
+    print('ulang:',nilai)
+print("==============")
+
